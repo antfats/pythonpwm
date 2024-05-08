8e4ef5c78ae56e64d04c9198d9bdb7865ef259f8
antfats <antfats@hotmail.com>
2024-05-08 12:34:17 -0700

    commit to add password funct, adding new update functional update password

--- passwordmanage.py
+++ passwordmanage.py
@@ -15,4 +15,16 @@ def _save_passwords(self):
     with open(self.storage_file,"w") as f:
         json.dump(self.passwords, f, indent=4)
 
+def add_password(self, service, username, password):
+    if service  in self.passwords: 
+        print(f"Service '{service}' already exists. Updating password...")
+        self.passwords[service] = {"username": username, "password": hashlib.sha256(password.encode()). hexdigest()}
+        self.save_passwords()
+        print (f"Password for '{service}' added/updated sucessfully.")
 
+def get_password(self,service): 
+    if service in self.passwords: 
+        return self.passwords[service]
+    else:
+        return None
+    
